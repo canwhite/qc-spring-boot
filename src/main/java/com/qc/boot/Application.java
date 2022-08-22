ea803445938699c338a37e28c3bbd788002747a3
qiaochao <qiaochao@icourt.cc>
2022-08-22 15:20:12 +0800

    feat : spring boot init

---
+++ src/main/java/com/qc/boot/Application.java
@@ -0,0 +1,35 @@
+package com.qc.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 功能描述: application
+ *
+ * @author lijinhua
+ * @date 2022/8/22 14:57
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // -- Mvc configuration ---------------------------------------------------
+
+    @Bean
+    WebMvcConfigurer createWebMvcConfigurer(@Autowired HandlerInterceptor[] interceptors) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
+    }
+}
