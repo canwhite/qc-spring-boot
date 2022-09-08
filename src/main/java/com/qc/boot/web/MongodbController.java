06addab9134015a2b1ce82237513c62c5cc5e5cf
qiaochao <qiaochao@icourt.cc>
2022-09-08 14:36:10 +0800

    feat : mongodb crud

--- src/main/java/com/qc/boot/web/MongodbController.java
+++ src/main/java/com/qc/boot/web/MongodbController.java
@@ -5,6 +5,7 @@ import com.qc.boot.entity.Students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -40,7 +41,32 @@ public class MongodbController {
     }
 
 
+    @GetMapping("/getStudentByName")
+    public Students getStudentByName(){
+         return  studentsDao.findStudentByName("zhangsan");
+    }
 
+    //更新
+    @GetMapping("/updateStudent")
+    public Students updateStudent(){
+        Students student = new Students();
+        student.setId(1L);
+        student.setUsername("zhangsan");
+        student.setPassword("715");
+
+        var num =  studentsDao.updateStudent(student);
+        System.out.println("num"+num);
+
+        return  student;
+    }
+
+    //删除
+    @GetMapping("/deleteStudent")
+    public Map<String,Object> deleteStudent(){
+        studentsDao.deleteStudentById(1L);
+        return  Map.of("msg","success");
+    }
+    
 
 
 
