e63593eef0907370b149efd067f69b0c3863c324
qiaochao <qiaochao@icourt.cc>
2022-12-27 15:19:46 +0800

    feat : zip service

--- src/main/java/com/qc/boot/service/ZipService.java
+++ src/main/java/com/qc/boot/service/ZipService.java
@@ -1,12 +1,37 @@
 package com.qc.boot.service;
+import java.io.File;
+import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @Service
 public class ZipService {
 
-    //完成一些聚合操作，这个没不要写repo了
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
 
-    
+    //上传操作
+    public void upload(MultipartFile file) throws IllegalStateException, IOException{
+        File dest = new File("/Users/zack/Desktop/path/to/save/" + file.getOriginalFilename());
+        //transferTo用于将上传的文件写入指定的路径
+        file.transferTo(dest);
+    }
+    //执行脚本，修改文件源地址
+    public void execShell(){
+        //TODO
+
+    }
+
+    //修改nginx
+    public void changeNginxConfig(){
+        //TODO
+
+
+    }
+
+
 }
--- src/main/java/com/qc/boot/web/ZipController.java
+++ src/main/java/com/qc/boot/web/ZipController.java
@@ -1,14 +1,14 @@
 package com.qc.boot.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.qc.boot.entity.CommonResult;
 import com.qc.boot.service.ZipService;
-
 import java.util.Map;
 import java.util.concurrent.Callable;
 
@@ -24,19 +24,39 @@ public class ZipController {
 
     //然后处理zip文件上传
     //easy function
-    @GetMapping("sendZip")
-    public Callable<CommonResult> sendZip(){
+    /**
+     * 
+     * curl 
+     *  -X POST 
+     *  -H "Content-Type: multipart/form-data" 
+     *  -F "file=@/Users/zack/Desktop/package.zip" 
+     *  -F "description=This is a description of the package" 
+     *  http://localhost:8080/zip/upload
+     */
+
+
+    @PostMapping("upload")
+    public Callable<CommonResult> sendZip(@RequestParam("file") MultipartFile file) {
         //返回callable,要看这是不是个异步操作了
         return  ()->{
+            
+            zipService.upload(file);
+
             CommonResult result = new CommonResult();
             result.setCode(200);
             result.setMsg("success");
-            result.setData(Map.of("result","success"));
-            
+            //返回文件地址
+            result.setData(Map.of("path",file.getOriginalFilename()));
+
+
             //callable内部返回值
             return result;
         };
     }
+
+
+
+
     
 
     
